ee135effb31a6e692a291a4ac9507c0a6965d11f
maykl4iv <mishaiv1995@gmail.com>
2019-04-29 21:18:27 +0200

    Updates

---
+++ precache-manifest.71ab62247d746af05e3a6daa45dcba1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdaae800c2defa73c763",
+    "url": "/robotfriends/static/js/runtime~main.fdaae800.js"
+  },
+  {
+    "revision": "dbc047ffab1ebed67fe6",
+    "url": "/robotfriends/static/js/main.dbc047ff.chunk.js"
+  },
+  {
+    "revision": "3f7fb6cfeaedbd773e5f",
+    "url": "/robotfriends/static/js/2.3f7fb6cf.chunk.js"
+  },
+  {
+    "revision": "dbc047ffab1ebed67fe6",
+    "url": "/robotfriends/static/css/main.1685f24d.chunk.css"
+  },
+  {
+    "revision": "3f7fb6cfeaedbd773e5f",
+    "url": "/robotfriends/static/css/2.4b158b9f.chunk.css"
+  },
+  {
+    "revision": "3f8a328768d3cc8e3d71076d9bc90a80",
+    "url": "/robotfriends/index.html"
+  }
+];
